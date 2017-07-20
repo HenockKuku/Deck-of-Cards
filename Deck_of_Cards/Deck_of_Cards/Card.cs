4a7ee9482778e6acf302542c74242aa3dfb03791
Henock <dadihenock@gmail.com>
2017-07-20 15:45:28 -0400

    Add files via upload

---
+++ Deck_of_Cards/Deck_of_Cards/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_of_Cards
+{
+  public class Card
+  {
+    private String face;
+    private String suit;
+    public Card (string cardFace, string cardSuit)
+    {
+      face = cardFace;
+      suit = cardSuit;
+    }
+    public override string ToString()
+    {
+      return face + " of " + suit ;
+    }
+  }
+}
---
+++ Deck_of_Cards/Deck_of_Cards/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_of_Cards
+{
+  public class MainProgram
+  {
+    public static void Main(string[] args)
+    {
+      Deck deck1 = new Deck();
+      deck1.Shuffle();
+      for (int i=0; i<52;i++)
+      {
+        Console.Write("{0,-19}", deck1.dealOneCard());
+        if ((i + 1) % 4 == 0)
+          Console.WriteLine();
+      }
+      Console.ReadLine();
+    }
+  }
+}
